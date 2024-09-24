7015f56501e2d67c99e3e5e1fe83fdaaf54ab23a
yafu <zhyaphoo@qq.com>
2024-09-24 18:01:48 +0800

    v2.1.0

--- constant/constant.go
+++ constant/constant.go
@@ -3,7 +3,7 @@ package constant
 const AppDir string = "push-me-client"
 const AppID string = "PushMeClient"
 const AppName string = "PushMe Client"
-const AppVersion string = "v2.0.3"
+const AppVersion string = "v2.1.0"
 const UrlVersion string = "https://push.i-i.me/api/version"
 
 const (
