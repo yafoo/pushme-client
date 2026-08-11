4a3fd5e4535a9900316718b6433c62c49c9d214c
yafu <zhyaphoo@qq.com>
2026-08-11 19:19:18 +0800

    修改便签时验证标题

--- internal/models/msg/msg.go
+++ internal/models/msg/msg.go
@@ -158,6 +158,13 @@ func Del(id int) bool {
 }
 
 func Update(data *db.Msg) bool {
+	if data.Type == "note" {
+		res := db.Msg{}
+		db.Db.Where("title like ?", data.Title).Order("id desc").First(&res)
+		if res.ID > 0 && res.ID != data.ID {
+			return false
+		}
+	}
 	result := db.Db.Save(data)
 	return result.Error == nil
 }
